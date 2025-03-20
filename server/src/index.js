1ac82c65162835c6defa788bbc9d3f4143335839
edenSaadon <edenSaadon98@gmail.com>
2025-03-20 16:36:31 +0200

    changes

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,11 @@
 //index.js מאזין לפורט.
 
 
-require('dotenv').config();
+//require('dotenv').config();
+const app = require("./app");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+});
